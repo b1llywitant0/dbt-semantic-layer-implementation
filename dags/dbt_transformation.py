6ce2002e16b374794feac2fbbd1de21bb8ffa7be
b1llywitant0 <billywitanto@yahoo.co.id>
2025-02-14 14:17:20 +0700

    Chore: Commenting dbt test DAG

--- dags/dbt_transformation.py
+++ dags/dbt_transformation.py
@@ -6,26 +6,26 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2025, 2, 13),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    # 'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG (
     'testing_dag', 
     default_args=default_args, 
-    schedule_interval=timedelta(minutes=5),
+    schedule_interval=None
 )
 
 dbt_test = BashOperator(
     task_id='dbt_test',
     bash_command='cd /opt/airflow/dbt && dbt test',
-    dag=dag,
+    # dag=dag,
 )
 
 dbt_run = BashOperator(
     task_id='dbt_run',
     bash_command='cd /opt/airflow/dbt && dbt run',
-    dag=dag
+    dag=dag,
 )
 
-dbt_run >> dbt_test
+dbt_run
