3088bb692ea8d7aad21ec99f142df24c37939d9a
b1llywitant0 <billywitanto@yahoo.co.id>
2025-02-21 14:40:05 +0700

    Fix: Add metadata into gitignore

--- clickhouse/store/9b2/9b2d9f63-d435-4c7b-b03d-b850c20fe483/testing.sql
+++
@@ -1,9 +0,0 @@
-ATTACH TABLE _ UUID 'dec01f39-419f-4cca-be45-c0220a226f53'
-(
-    `id` int16,
-    `name` String
-)
-ENGINE = MergeTree
-PRIMARY KEY tuple(id)
-ORDER BY tuple(id)
-SETTINGS index_granularity = 8192
