ebaef85bdacc280da7133611698be64382194d2b
b1llywitant0 <billywitanto@yahoo.co.id>
2025-03-06 19:47:14 +0700

    Feat: DAG for batch load from Postgre to ClickHouse

--- airflow/dags/transfer_reference_tables.py
+++ airflow/dags/transfer_reference_tables.py
@@ -4,12 +4,11 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
-
 def sqlite_to_clickhouse():
     postgres_hook = PostgresHook(postgres_conn_id='postgres_ecommerce_db')
-    ch_hook = ClickHouseHook()
+    ch_hook = ClickHouseHook(clickhouse_conn_id='clickhouse_ecommerce_dw')
     records = postgres_hook.get_records('SELECT * FROM product_categories')
-    print(records)
+    ch_hook.execute('INSERT INTO product_categories VALUES', records)
 
 with DAG(
         dag_id='sqlite_to_clickhouse',
