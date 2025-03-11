914b917474d86408e865264130729e9a947cbe71
b1llywitant0 <billywitanto@yahoo.co.id>
2025-03-11 14:46:13 +0700

    Refactor: Incremental model for base products to handle growing data

--- dbt_data/models/staging/olist/base/base_olist__products.sql
+++ dbt_data/models/staging/olist/base/base_olist__products.sql
@@ -1,8 +1,18 @@
-WITH
-current_products AS (
-    SELECT *
-    FROM {{ source('olist','mv_products') }}
-    FINAL
-)
+-- Handling duplicate records created by handling late data by using unique_key
+{{ 
+    config(engine='MergeTree()', 
+    materialized='incremental', 
+    unique_key='product_id')
+}}
 
-SELECT * FROM current_products
+SELECT *
+FROM {{ source('olist','mv_products') }}
+
+-- Also handling late data, if exists
+{% if is_incremental %}
+    WHERE updated_at >= ( 
+        SELECT addDays(MAX(updated_at), -3) from {{ this }}
+    )
+{% endif %}
+
+-- Once a week, run full refresh
