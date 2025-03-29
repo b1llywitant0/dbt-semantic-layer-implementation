2f02480710a34140af3f32b7167b082f253a6856
b1llywitant0 <billywitanto@yahoo.co.id>
2025-03-29 22:43:13 +0700

    Feat: dbt model - Order customer mapping

---
+++ dbt_data/models/intermediate/int_ecomm__order_customer_map.sql
@@ -0,0 +1,22 @@
+{{ 
+    config(
+        order_by='(customer_id, order_purchase_timestamp, order_id)' 
+        )
+}}
+
+WITH
+order_customer AS (
+    SELECT
+        customer_id,
+        order_id,
+        order_purchase_timestamp,
+        toYear(order_purchase_timestamp) AS order_year,
+        toMonth(order_purchase_timestamp) AS order_month
+    FROM {{ ref("stg_olist__orders") }}
+    WHERE valid_to = '{{var("future_proof_date")}}'
+)
+
+SELECT * FROM order_customer
+
+
+
