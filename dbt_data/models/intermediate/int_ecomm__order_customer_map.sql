32e2a3df2f2120e58ab9cf89fce63553157758db
b1llywitant0 <billywitanto@yahoo.co.id>
2025-03-30 03:07:00 +0700

    Feat: Customer retention model in dbt

--- dbt_data/models/intermediate/int_ecomm__order_customer_map.sql
+++ dbt_data/models/intermediate/int_ecomm__order_customer_map.sql
@@ -1,6 +1,6 @@
 {{ 
     config(
-        order_by='(customer_id, order_purchase_timestamp, order_id)' 
+        order_by='(order_year, order_month, user_id, order_purchase_timestamp, order_id)' 
         )
 }}
 
@@ -14,9 +14,27 @@ order_customer AS (
         toStartOfMonth(order_purchase_timestamp) AS order_month
     FROM {{ ref("stg_olist__orders") }}
     WHERE valid_to = '{{var("future_proof_date")}}'
+),
+
+valid_orders AS (
+    SELECT *
+    FROM {{ ref("int_ecomm__valid_orders_based_on_order_items") }}
+),
+
+users AS (
+    SELECT
+        account_id,
+        user_id
+    FROM {{ ref("stg_olist__customers") }}
+    WHERE valid_to = '{{var("future_proof_date")}}'
 )
 
-SELECT * FROM order_customer
-
-
-
+SELECT 
+    users.user_id,
+    valid_orders.order_id AS order_id,
+    order_customer.order_purchase_timestamp,
+    order_customer.order_year,
+    order_customer.order_month
+FROM valid_orders
+LEFT JOIN order_customer ON order_customer.order_id = valid_orders.order_id
+LEFT JOIN users ON order_customer.customer_id = users.account_id
---
+++ dbt_data/models/marts/ecommerce/customer_retention.sql
@@ -0,0 +1,77 @@
+WITH
+    start_date AS (
+      SELECT toStartOfMonth(min(order_purchase_timestamp)) 
+      FROM {{ ref("int_ecomm__order_customer_map") }}
+    ),
+    end_date AS (
+      SELECT toStartOfMonth(max(order_purchase_timestamp)) 
+      FROM {{ ref("int_ecomm__order_customer_map") }}
+    ),
+    period AS (
+        SELECT DISTINCT date_month 
+        FROM {{ ref("dim_dates") }}
+        WHERE date_day BETWEEN (SELECT * FROM start_date) AND (SELECT * FROM end_date)
+    ),
+    
+    crossed AS (
+        SELECT 
+            DISTINCT int_ecomm__order_customer_map.user_id,
+            period.date_month
+        FROM {{ ref("int_ecomm__order_customer_map") }}
+        CROSS JOIN period
+    ),
+    
+    customer_activity AS (
+        SELECT
+            DISTINCT user_id,
+            order_month
+        FROM {{ ref("int_ecomm__order_customer_map") }}
+    ),
+    
+    joined AS (
+        SELECT
+            crossed.user_id AS user_id,
+            crossed.date_month AS date_month,
+            customer_activity.order_month AS order_month
+        FROM crossed
+        LEFT JOIN customer_activity ON 
+            crossed.user_id = customer_activity.user_id AND
+            crossed.date_month = customer_activity.order_month
+    ),
+    
+    windowed AS (
+        SELECT *,
+        leadInFrame(order_month, 1) OVER (PARTITION BY user_id ORDER BY date_month ASC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS month_plus_1,
+        leadInFrame(order_month, 2) OVER (PARTITION BY user_id ORDER BY date_month ASC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS month_plus_2,
+        leadInFrame(order_month, 3) OVER (PARTITION BY user_id ORDER BY date_month ASC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS month_plus_3,
+        leadInFrame(order_month, 4) OVER (PARTITION BY user_id ORDER BY date_month ASC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS month_plus_4,
+        leadInFrame(order_month, 5) OVER (PARTITION BY user_id ORDER BY date_month ASC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS month_plus_5,
+        leadInFrame(order_month, 6) OVER (PARTITION BY user_id ORDER BY date_month ASC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS month_plus_6
+        FROM joined
+        WHERE order_month IS NOT NULL
+    )
+    
+SELECT
+    date_month AS month,
+    COUNT(DISTINCT user_id) AS active_users,
+    COUNT(DISTINCT CASE WHEN month_plus_1 IS NOT NULL THEN user_id END) AS retention_month_1,
+    COUNT(DISTINCT CASE WHEN month_plus_2 IS NOT NULL THEN user_id END) AS retention_month_2,
+    COUNT(DISTINCT CASE WHEN month_plus_3 IS NOT NULL THEN user_id END) AS retention_month_3,
+    COUNT(DISTINCT CASE WHEN month_plus_4 IS NOT NULL THEN user_id END) AS retention_month_4,
+    COUNT(DISTINCT CASE WHEN month_plus_5 IS NOT NULL THEN user_id END) AS retention_month_5,
+    COUNT(DISTINCT CASE WHEN month_plus_6 IS NOT NULL THEN user_id END) AS retention_month_6
+FROM windowed
+GROUP BY date_month
+ORDER BY date_month
