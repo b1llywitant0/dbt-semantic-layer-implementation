608cbf3e8b43b732026b2b1c10f93f81e9140ded
b1llywitant0 <billywitanto@yahoo.co.id>
2025-04-15 13:25:41 +0700

    Refactor: Using TriggerDagRunOperator

--- airflow/dags/dbt_transformation.py
+++ airflow/dags/dbt_transformation.py
@@ -1,31 +1,40 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+import pytz
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2025, 2, 13),
+    'start_date': datetime(2025, 3, 6, tzinfo=pytz.timezone("Asia/Jakarta")),
     # 'retries': 1,
     # 'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG (
-    'testing_dag', 
+    'dbt_transformation', 
     default_args=default_args, 
-    schedule_interval=None
+    schedule_interval='0 0 * * *',
+    catchup=False
 )
 
-dbt_test = BashOperator(
-    task_id='dbt_test',
-    bash_command='cd /opt/airflow/dbt && dbt test',
-    # dag=dag,
-)
-
-dbt_run = BashOperator(
-    task_id='dbt_run',
-    bash_command='cd /opt/airflow/dbt && dbt run',
+dbt_deps = BashOperator(
+    task_id='dbt_deps',
+    bash_command='cd /opt/airflow/dbt && dbt deps',
     dag=dag,
 )
 
-dbt_run
+dbt_build = BashOperator(
+    task_id='dbt_build',
+    bash_command='cd /opt/airflow/dbt && dbt build',
+    dag=dag,
+)
+
+load_reference_tables = TriggerDagRunOperator(
+    task_id='load_reference_tables',
+    trigger_dag_id='reference_tables_postgres_to_clickhouse',
+    wait_for_completion=True,
+)
+
+dbt_deps >> load_reference_tables >> dbt_build
